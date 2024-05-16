2d43f025a3b1b53585926bcb8645e48d4b9bace8
Diego Sanz <digosanf@gmail.com>
2024-05-16 18:37:38 +0200

    Failed propagation attempt

--- src_cuda/test.cpp
+++ src_cuda/test.cpp
@@ -11,12 +11,67 @@
 
 #include <complex>
 #include <iostream>
+#include "vector"
+#include "algorithm"
+
+// https://stackoverflow.com/questions/5915125/fftshift-ifftshift-c-c-source-code
+static inline
+void fftshift2D(std::complex<double> *data, size_t xdim, size_t ydim) {
+    size_t xshift = xdim / 2;
+    size_t yshift = ydim / 2;
+    if ((xdim * ydim) % 2 != 0) {
+        // temp output array
+        std::vector<std::complex<double> > out;
+        out.resize(xdim * ydim);
+        for (size_t x = 0; x < xdim; x++) {
+            size_t outX = (x + xshift) % xdim;
+            for (size_t y = 0; y < ydim; y++) {
+                size_t outY = (y + yshift) % ydim;
+                // row-major order
+                out[outX + xdim * outY] = data[x + xdim * y];
+            }
+        }
+        // copy out back to data
+        copy(out.begin(), out.end(), &data[0]);
+    } else {
+        // in and output array are the same,
+        // values are exchanged using swap
+        for (size_t x = 0; x < xdim; x++) {
+            size_t outX = (x + xshift) % xdim;
+            for (size_t y = 0; y < yshift; y++) {
+                size_t outY = (y + yshift) % ydim;
+                // row-major order
+                swap(data[outX + xdim * outY], data[x + xdim * y]);
+            }
+        }
+    }
+}
+
+// https://stackoverflow.com/questions/5915125/fftshift-ifftshift-c-c-source-code
+template<class ty>
+void circshift(const ty *in, ty *out, int xdim, int ydim, int xshift, int yshift) {
+    for (int i = 0; i < xdim; i++) {
+        int ii = (i + xshift) % xdim;
+        for (int j = 0; j < ydim; j++) {
+            int jj = (j + yshift) % ydim;
+            out[ii * ydim + jj] = in[i * ydim + j];
+        }
+    }
+}
+
+void fftshift(std::complex<double> *data, int xdim, int ydim) {
+    auto *tmp = new std::complex<double>[xdim * ydim];
+    circshift(data, tmp, xdim, ydim, xdim / 2, ydim / 2);
+    copy(tmp, tmp + xdim * ydim, data);
+    free(tmp);
+}
+
 
 void angular_spectrum_kernel(fftw_complex *in, fftw_complex *out) {
     int num_pixels_x = 1920;
     int num_pixels_y = 1080;
     double pixel_size = 8e-6;
-    double z = -0.2;
+    double z = -0.1;
     double wavelength = 0.6328e-6;
 
     double k = 2 * M_PI / wavelength;
@@ -33,11 +88,11 @@ void angular_spectrum_kernel(fftw_complex *in, fftw_complex *out) {
     // fy = (wavelength/slm_size_y) * (np.arange(num_pixels_y) - num_pixels_y // 2)
     double fx[num_pixels_x];
     for (int i = 0; i < num_pixels_x; ++i) {
-        fx[i] = a * (i - num_pixels_x / 2);
+        fx[i] = a * (i - std::floor(num_pixels_x / 2.0));
     }
     double fy[num_pixels_y];
     for (int i = 0; i < num_pixels_y; ++i) {
-        fy[i] = b * (i - num_pixels_y / 2);
+        fy[i] = b * (i - std::floor(num_pixels_y / 2.0));
     }
 
     // fxx, fyy = np.meshgrid(fx, fy)
@@ -61,29 +116,17 @@ void angular_spectrum_kernel(fftw_complex *in, fftw_complex *out) {
 
     // kernel = np.exp(1j*((k*z)*np.sqrt(1-mod)))
     fftw_complex *tmp = fftw_alloc_complex(size);
-    fftw_complex *kernel_fft = fftw_alloc_complex(size);
+    auto *kernel = new std::complex<double>[size];
 
     for (int i = 0; i < size; ++i) {
-        auto c = std::exp(static_cast<std::complex<double>>(1.0j * (k * z) * std::sqrt(1 - mod[i])));
-        kernel_fft[i][0] = c.real();
-        kernel_fft[i][1] = c.imag();
+        auto c = std::exp(std::complex<double>(0, (k * z) * std::sqrt(1 - mod[i])));
+        kernel[i] = c;
     }
 
     // kernel = fftshift(kernel)
-    for (int i = 0; i < size; ++i) {
-        fftw_complex tmp_c;
-        int src = i;
-        int dst = (i + size/2 - 1) % size;
-
-        tmp_c[0] = kernel_fft[src][0];
-        tmp_c[1] = kernel_fft[src][1];
-
-        kernel_fft[src][0] = kernel_fft[dst][0];
-        kernel_fft[src][1] = kernel_fft[dst][1];
-
-        kernel_fft[dst][0] = tmp_c[0];
-        kernel_fft[dst][1] = tmp_c[1];
-    }
+    printf("kernel 0 0: %f %f\n", kernel[0].real(), kernel[0].imag());
+    fftshift(kernel, num_pixels_x, num_pixels_y);
+    printf("kernel 0 0: %f %f\n", kernel[0].real(), kernel[0].imag());
 
     // Propagate_wave = np.fft.ifft2(fft2(input_image)*kernel)
     auto plan = fftw_plan_dft_2d(num_pixels_x, num_pixels_y, in, tmp, FFTW_FORWARD, FFTW_ESTIMATE);
@@ -91,25 +134,25 @@ void angular_spectrum_kernel(fftw_complex *in, fftw_complex *out) {
     fftw_destroy_plan(plan);
 
     for (int i = 0; i < size; ++i) {
-        tmp[i][0] *= kernel_fft[i][0];
-        tmp[i][1] *= kernel_fft[i][1];
+        tmp[i][0] *= kernel[i].real() / size;
+        tmp[i][1] *= kernel[i].imag() / size;
     }
 
     plan = fftw_plan_dft_2d(num_pixels_x, num_pixels_y, tmp, out, FFTW_BACKWARD, FFTW_ESTIMATE);
     fftw_execute(plan);
     fftw_destroy_plan(plan);
 
-    fftw_free(kernel_fft);
+    fftw_free(tmp);
     free(fxx);
+    free(kernel);
     free(fyy);
     free(mod);
-    free(tmp);
 }
 
 int main() {
     int w, h, comp;
 
-    unsigned char *image = stbi_load("test.png", &w, &h, &comp, STBI_grey);
+    unsigned char *image = stbi_load("ph_1cpu_166s.png", &w, &h, &comp, STBI_grey);
     printf("%d %d %d\n", w, h, comp);
     std::flush(std::cout);
 
@@ -118,20 +161,27 @@ int main() {
     fftw_complex *in = fftw_alloc_complex(size);
     fftw_complex *out = fftw_alloc_complex(size);
 
+
+    // [0, 255] -> [-1, 1] -> [-pi, pi]
     for (int i = 0; i < size; ++i) {
-        in[i][0] = static_cast<double>(image[i]) / 255;
-        in[i][1] = 0;
+        auto c = std::exp(std::complex<double>(0, M_PI * (image[i] - 127.5) / 127.5));
+        in[i][0] = c.real();
+        in[i][1] = c.imag();
     }
+    printf("in 0 0: %f %f\n", in[0][0], in[0][1]);
 
     angular_spectrum_kernel(in, out);
 
+    printf("out 0 0: %f %f\n", out[0][0], out[0][1]);
+    printf("pp 0 0: %f\n", std::abs(std::complex{out[0][0], out[0][1]}));
     for (int i = 0; i < size; ++i) {
-        image[i] = static_cast<unsigned char>(out[i][0] / size * 255);
+        std::complex c = {out[i][0], out[i][1]};
+        image[i] = static_cast<unsigned char>(std::abs(c) * 255);
     }
 
     stbi_write_png("test_out1.png", w, h, 1, image, 0);
 
-    stbi_write_png("test_out1.png", w, h, 1, image, 0);
+    //stbi_write_png("test_out1.png", w, h, 1, image, 0);
 
     fftw_free(in);
     fftw_free(out);
