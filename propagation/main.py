e10852039fdf1900da76b503d30da792515e7386
Diego Sanz <digosanf@gmail.com>
2024-05-17 20:46:22 +0200

    Improve propagation visualization

--- propagation/main.py
+++ propagation/main.py
@@ -4,7 +4,6 @@ from PIL import Image
 from numpy.fft.helper import fftshift
 from numpy.fft import fft2, ifft2
 
-
 mm = 1e-3
 um = 1e-6
 nm = 1e-9
@@ -12,7 +11,7 @@ nm = 1e-9
 slm_z = -1000 * mm
 pixelsize = 8 * um
 wl = 632.8 * nm
-k = 2 * np.pi/wl
+k = 2 * np.pi / wl
 
 # z_srt = input('Propagation distance, z (mm): ')
 
@@ -26,7 +25,7 @@ dy = pixelsize
 
 # Import CGH
 
-image = Image.open('../out/ph_16cpu_387s.png')
+image = Image.open('../out/ph_16cpu_1613s.png')
 data_im = np.array(image)
 
 print("Imported image: ", data_im.shape, " pixels.")
@@ -41,34 +40,33 @@ print("in 0 0:", data_phase[0, 0].real, data_phase[0, 0].imag)
 nx = int(data_phase.shape[1])
 ny = int(data_phase.shape[0])
 
-lx = nx*dx
-ly = ny*dy
+lx = nx * dx
+ly = ny * dy
+
 
 # Propagation kernel
 
 
 def propagation_kernel(slm_z):
-    fx = (wl/lx) * (np.arange(nx) - nx // 2)
-    fy = (wl/ly) * (np.arange(ny) - ny // 2)
+    fx = (wl / lx) * (np.arange(nx) - nx // 2)
+    fy = (wl / ly) * (np.arange(ny) - ny // 2)
 
     fxx, fyy = np.meshgrid(fx, fy)
 
     mod_fxfy = fxx * fxx + fyy * fyy
-    kernel = np.exp(1j*((k*slm_z)*np.sqrt(1-mod_fxfy)))
+    kernel = np.exp(1j * ((k * slm_z) * np.sqrt(1 - mod_fxfy)))
     print("kernel 0 0:", kernel[0, 0])
     kernel = fftshift(kernel)
     print("kernel 0 0:", kernel[0, 0])
 
-    propagated = ifft2(fft2(data_phase)*kernel)
+    propagated = ifft2(fft2(data_phase) * kernel)
 
     return propagated
 
 
-p = propagation_kernel(-100*mm)
-print("out 0 0: ", p[0, 0].real, p[0, 0].imag)
-pp = np.abs(p)
-print("pp: ", pp[0, 0])
-# exit(0)
+plt.imsave('propagated_image_200.png', np.abs(propagation_kernel(-200 * mm)), cmap='gray')
+plt.imsave('propagated_image_212.png', np.abs(propagation_kernel(-212 * mm)), cmap='gray')
+
 # Show an image with pyplot
 
 zs = [200, 400, 600, 800, 1000, 1200]
@@ -80,11 +78,10 @@ max_columns = 3
 num_rows = int(np.ceil(len(zs) / max_columns))
 num_cols = min(len(zs), max_columns)
 
-print(f"Plotting {len(zs)} images in a grid of {
-      num_rows} rows and {num_cols} columns.")
+print(f"Plotting {len(zs)} images in a grid of {num_rows} rows and {num_cols} columns.")
 
 fig, axes = plt.subplots(
-    num_rows, num_cols, figsize=(5 * num_cols, 3*num_rows))
+    num_rows, num_cols, figsize=(16 * num_cols // 2, 9 * num_rows // 2))
 if num_rows == 1:
     axes = np.reshape(axes, (1, -1))
 elif num_cols == 1:
@@ -92,12 +89,19 @@ elif num_cols == 1:
 
 for i, ax in enumerate(axes.flat):
     if i < len(zs):
-        ax.imshow(np.abs(propagation_kernel(-zs[i]*mm)), cmap='gray')
+        ax.imshow(np.abs(propagation_kernel(-zs[i] * mm)), cmap='gray')
         ax.axis('off')
         ax.set_title('z = {} mm'.format(zs[i]))
+        width = data_im.shape[1]
+        height = data_im.shape[0]
+        zoom_factor = 2
+        x_center, y_center = width // 2, height // 2  # Center of the image
+        zoom_width, zoom_height = width // zoom_factor, height // zoom_factor
+        ax.set_xlim(x_center - zoom_width // 2, x_center + zoom_width // 2)
+        ax.set_ylim(y_center + zoom_height // 2, y_center - zoom_height // 2)  # Inverted y-axis for images
     else:
         ax.axis('off')
 
 plt.tight_layout()
+plt.savefig("figure.png")
 plt.show()
-plt.savefig("out.png")
